1b84bbbbacdb80c44c89ac265e60ea5c838844c1
KyoZeus <franciscopaivaknebel@gmail.com>
2016-07-10 21:15:41 -0300

    Added sample apps.

---
+++ sampleApps/app1/app1.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 8001;
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, 'localhost', function(){
+	console.log("Listening on port " + port);
+});
